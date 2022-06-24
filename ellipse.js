a2f377a0494bbf45b1e91e5c2415085917716d26
Александр Степанов <ab3000645@gmail.com>
2022-06-24 19:02:40 +0300

    added ellipse.js

---
+++ ellipse.js
@@ -0,0 +1,70 @@
+const ellipseContourColor = "#0E1B0B";
+const ellipseContourWidth = 3;
+
+class Ellipse
+{
+    constructor(position, direction, sizeA, sizeB)
+    {
+        direction = direction.normalized;
+        let normal = new Vector2(-direction.y, direction.x);
+        this.center = Controls.Add(position);
+        this.a1 = Controls.Add(position.Add(direction.Mul(sizeA)));
+        this.a2 = Controls.Add(position.Add(direction.Mul(-sizeA)));
+        this.b1 = Controls.Add(position.Add(normal.Mul(sizeB)));
+        this.b2 = Controls.Add(position.Add(normal.Mul(-sizeB)));
+        this.sizeA = sizeA;
+        this.sizeB = sizeB;
+    }
+
+    Draw()
+    {
+        ctx.strokeStyle = ellipseContourColor;
+        ctx.lineWidth = ellipseContourWidth;
+        ctx.beginPath();
+        ctx.ellipse(CONTROLS[this.center].x, CONTROLS[this.center].y, this.sizeA, this.sizeB, CONTROLS[this.a1].Sub(CONTROLS[this.center]).angle, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+
+    HandleChangedControl(id, oldPosition, newPosition)
+    {
+        switch(id)
+        {
+            case this.center:
+                let delta = newPosition.Sub(oldPosition);
+                CONTROLS[this.a1] = CONTROLS[this.a1].Add(delta);
+                CONTROLS[this.a2] = CONTROLS[this.a2].Add(delta);
+                CONTROLS[this.b1] = CONTROLS[this.b1].Add(delta);
+                CONTROLS[this.b2] = CONTROLS[this.b2].Add(delta);
+                break;
+            case this.a1:
+                CONTROLS[this.a2] = CONTROLS[this.center].Mul(2).Sub(CONTROLS[this.a1]);
+                break;
+            case this.a2:
+                CONTROLS[this.a1] = CONTROLS[this.center].Mul(2).Sub(CONTROLS[this.a2]);
+                break;
+            case this.b1:
+                CONTROLS[this.b2] = CONTROLS[this.center].Mul(2).Sub(CONTROLS[this.b1]);
+                break;
+            case this.b2:
+                CONTROLS[this.b1] = CONTROLS[this.center].Mul(2).Sub(CONTROLS[this.b2]);
+                break;
+        }
+
+        if(id == this.a1 || id == this.a2)
+        {
+            let dir = CONTROLS[this.a1].Sub(CONTROLS[this.center]);
+            this.sizeA = dir.len;
+            let normal = (new Vector2(dir.y, -dir.x)).normalized;
+            CONTROLS[this.b1] = CONTROLS[this.center].Add(normal.Mul(this.sizeB));
+            CONTROLS[this.b2] = CONTROLS[this.center].Add(normal.Mul(-this.sizeB));
+        }
+        else if(id == this.b1 || id == this.b2)
+        {
+            let dir = CONTROLS[this.b1].Sub(CONTROLS[this.center]);
+            this.sizeB = dir.len;
+            let normal = (new Vector2(dir.y, -dir.x)).normalized;
+            CONTROLS[this.a1] = CONTROLS[this.center].Add(normal.Mul(this.sizeA));
+            CONTROLS[this.a2] = CONTROLS[this.center].Add(normal.Mul(-this.sizeA));
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const ellipseContourColor = "#0E1B0B";
-const ellipseContourWidth = 3;
-
 function ResizeCanvas()
 {
     cnv.width = window.innerWidth - 16;
@@ -15,74 +12,22 @@ window.addEventListener('resize', e =>
     Redraw();
 });
 
-var first_center = Controls.Add(new Vector2(500, 400));
-var first_a1 = Controls.Add(new Vector2(700, 400));
-var first_a2 = Controls.Add(new Vector2(300, 400));
-var first_b1 = Controls.Add(new Vector2(500, 300));
-var first_b2 = Controls.Add(new Vector2(500, 500));
-var size_a = CONTROLS[first_center].Sub(CONTROLS[first_a1]).len;
-var size_b = CONTROLS[first_center].Sub(CONTROLS[first_b1]).len;
+var firstEllipse = new Ellipse(new Vector2(500, 400), new Vector2(1, 0), 200, 100);
+var secondEllipse = new Ellipse(new Vector2(600, 600), new Vector2(1, 0), 200, 100);
 
 function Redraw()
 {
     ctx.clearRect(0, 0, cnv.width, cnv.height);
-
-    ctx.strokeStyle = ellipseContourColor;
-    ctx.lineWidth = ellipseContourWidth;
-    ctx.beginPath();
-    ctx.ellipse(CONTROLS[first_center].x, CONTROLS[first_center].y, size_a, size_b, CONTROLS[first_a1].Sub(CONTROLS[first_center]).angle, 0, 2 * Math.PI);
-    ctx.stroke();
-
+    firstEllipse.Draw();
+    secondEllipse.Draw();
     Controls.Draw();
 }
 
 Redraw();
 
-function HandleEllipse(id, oldPosition, newPosition, center, a1, a2, b1, b2)
-{
-    let delta = newPosition.Sub(oldPosition);
-
-    switch(id)
-    {
-        case center:
-            CONTROLS[a1] = CONTROLS[a1].Add(delta);
-            CONTROLS[a2] = CONTROLS[a2].Add(delta);
-            CONTROLS[b1] = CONTROLS[b1].Add(delta);
-            CONTROLS[b2] = CONTROLS[b2].Add(delta);
-            break;
-        case a1:
-            CONTROLS[a2] = CONTROLS[center].Mul(2).Sub(CONTROLS[a1]);
-            break;
-        case a2:
-            CONTROLS[a1] = CONTROLS[center].Mul(2).Sub(CONTROLS[a2]);
-            break;
-        case b1:
-            CONTROLS[b2] = CONTROLS[center].Mul(2).Sub(CONTROLS[b1]);
-            break;
-        case b2:
-            CONTROLS[b1] = CONTROLS[center].Mul(2).Sub(CONTROLS[b2]);
-            break;
-    }
-
-    if(id == a1 || id == a2)
-    {
-        let dir = CONTROLS[a1].Sub(CONTROLS[center]);
-        size_a = dir.len;
-        CONTROLS[b1] = CONTROLS[center].Add((new Vector2(dir.y, -dir.x)).normalized.Mul(size_b));
-        CONTROLS[b2] = CONTROLS[center].Add((new Vector2(-dir.y, dir.x)).normalized.Mul(size_b));
-    }
-    else if(id == b1 || id == b2)
-    {
-        let dir = CONTROLS[b1].Sub(CONTROLS[center]);
-        size_b = dir.len;
-        CONTROLS[a1] = CONTROLS[center].Add((new Vector2(dir.y, -dir.x)).normalized.Mul(size_a));
-        CONTROLS[a2] = CONTROLS[center].Add((new Vector2(-dir.y, dir.x)).normalized.Mul(size_a));
-    }
-}
-
 Controls.controlChangedHandler = (id, oldPosition, newPosition) => 
 {
-    HandleEllipse(id, oldPosition, newPosition, first_center, first_a1, first_a2, first_b1, first_b2);
-
+    firstEllipse.HandleChangedControl(id, oldPosition, newPosition);
+    secondEllipse.HandleChangedControl(id, oldPosition, newPosition);
     Redraw();
 };
